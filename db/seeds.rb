2e20be679291ea4317dedb54afb553014e24ce1a
Edward Wu <wuedward@bitbucket.org>
2015-10-22 21:19:03 -0400

    seeding tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Tag.create(tag_name: "Magic: The Gathering", photo_url: "magic.jpeg")
+Tag.create(tag_name: "Backgammon", photo_url: "backgammon.jpeg")
+Tag.create(tag_name: "Cards Against Humanity", photo_url: "cards_against_humanity.jpeg")
+Tag.create(tag_name: "Poker", photo_url: "poker.jpeg")
+Tag.create(tag_name: "Mahjong", photo_url: "mahjong.jpeg")
+Tag.create(tag_name: "Dungeons & Dragons", photo_url: "dungeons_and_dragons.jpeg")
+Tag.create(tag_name: "Catan", photo_url: "settlers_of_catan.jpeg")
+Tag.create(tag_name: "Scrabble", photo_url: "scrabble.jpeg")
+Tag.create(tag_name: "Chess", photo_url: "chess.jpeg")
